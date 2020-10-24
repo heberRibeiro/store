7ce6aa7d211e5bc6d54c13983a9ee85c16b68a51
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-10-23 21:26:37 -0300

    Faq entity service layer

---
+++ src/main/java/br/unit/pe/store/services/FaqService.java
@@ -0,0 +1,68 @@
+package br.unit.pe.store.services;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import br.unit.pe.store.domain.Faq;
+import br.unit.pe.store.repositories.FaqRepository;
+import br.unit.pe.store.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class FaqService {
+	
+	@Autowired
+	private FaqRepository repository;
+	
+	public List<Faq> findAll(Pageable pageable) {
+		return repository.findAll(pageable).getContent();
+	}
+	
+	public Faq findById(Integer id) {
+
+		try {
+			Faq faq = repository.findById(id).get();
+			return faq;
+
+		} catch (NoSuchElementException e) {
+			throw new ObjectNotFoundException("Faq não encotrado na base de dados.");
+		}
+	}
+
+	public Faq insert(Faq faq) {
+		return repository.save(faq);
+	}
+
+	public Faq update(Faq faq, Integer id) {
+
+		try {
+			Faq obj = repository.getOne(id); // return a reference to the entity
+			obj.setId(faq.getId());
+			obj.setTexto(faq.getTexto());
+			obj.setDataHora(faq.getDataHora());
+			obj.setProdutoId(faq.getProdutoId());
+			
+			return repository.save(obj);
+
+		} catch (EntityNotFoundException e) {
+			throw new ObjectNotFoundException("Faq não encontrada na base de dados para atualização.");
+		}
+
+	}
+
+	public void delete(Integer id) {
+		try {
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundException("Faq não encontrada na base de dados para deleção.");
+		}
+	}
+
+}
