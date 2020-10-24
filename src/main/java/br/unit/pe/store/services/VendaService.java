18b17adbf243f9e34686629a582ed655d66eed78
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-10-23 21:57:17 -0300

    Venda entity sevice layer

---
+++ src/main/java/br/unit/pe/store/services/VendaService.java
@@ -0,0 +1,68 @@
+package br.unit.pe.store.services;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import br.unit.pe.store.domain.Venda;
+import br.unit.pe.store.repositories.VendaRepository;
+import br.unit.pe.store.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class VendaService {
+
+	@Autowired
+	private VendaRepository repository;
+	
+	public List<Venda> findAll(Pageable pageable) {
+		return repository.findAll(pageable).getContent();
+	}
+	
+	public Venda findById(Integer id) {
+
+		try {
+			Venda venda = repository.findById(id).get();
+			return venda;
+
+		} catch (NoSuchElementException e) {
+			throw new ObjectNotFoundException("Venda não encotrada na base de dados.");
+		}
+	}
+
+	public Venda insert(Venda venda) {
+		return repository.save(venda);
+	}
+
+	public Venda update(Venda venda, Integer id) {
+
+		try {
+			Venda obj = repository.getOne(id); // return a reference to the entity
+			obj.setId(venda.getId());
+			obj.setData(venda.getData());
+			obj.setClientId(venda.getClientId());
+			obj.setFormaPagamentoId(venda.getFormaPagamentoId());
+			obj.setTotal(venda.getTotal());
+			
+			return repository.save(obj);
+
+		} catch (EntityNotFoundException e) {
+			throw new ObjectNotFoundException("Venda não encontrada na base de dados para atualização.");
+		}
+
+	}
+
+	public void delete(Integer id) {
+		try {
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundException("Venda não encontrada na base de dados para deleção.");
+		}
+	}
+}
