af6c9ba45a9b8bdf2a87832a89c6dac53585d3ab
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-10-23 21:46:53 -0300

    Marca entity service layer

---
+++ src/main/java/br/unit/pe/store/services/MarcaService.java
@@ -0,0 +1,67 @@
+package br.unit.pe.store.services;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import br.unit.pe.store.domain.Marca;
+import br.unit.pe.store.repositories.MarcaRepository;
+import br.unit.pe.store.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class MarcaService {
+	
+	@Autowired
+	private MarcaRepository repository;
+	
+	public List<Marca> findAll(Pageable pageable) {
+		return repository.findAll(pageable).getContent();
+	}
+	
+	public Marca findById(Integer id) {
+
+		try {
+			Marca marca = repository.findById(id).get();
+			return marca;
+
+		} catch (NoSuchElementException e) {
+			throw new ObjectNotFoundException("Marca não encotrada na base de dados.");
+		}
+	}
+
+	public Marca insert(Marca marca) {
+		return repository.save(marca);
+	}
+
+	public Marca update(Marca marca, Integer id) {
+
+		try {
+			Marca obj = repository.getOne(id); // return a reference to the entity
+			obj.setId(marca.getId());
+			obj.setNome(marca.getNome());
+			obj.setDescricao(marca.getDescricao());
+			
+			return repository.save(obj);
+
+		} catch (EntityNotFoundException e) {
+			throw new ObjectNotFoundException("Marca não encontrada na base de dados para atualização.");
+		}
+
+	}
+
+	public void delete(Integer id) {
+		try {
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundException("Marca não encontrado na base de dados para deleção.");
+		}
+	}
+
+}
