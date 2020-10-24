dd0d0c9a2a6c70487e7b49d1dabbbb61a19b79da
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-10-23 21:51:28 -0300

    Produto entity service layer

---
+++ src/main/java/br/unit/pe/store/services/ProdutoService.java
@@ -0,0 +1,71 @@
+package br.unit.pe.store.services;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import br.unit.pe.store.domain.Produto;
+import br.unit.pe.store.repositories.ProdutoRepository;
+import br.unit.pe.store.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repository;
+	
+	public List<Produto> findAll(Pageable pageable) {
+		return repository.findAll(pageable).getContent();
+	}
+	
+	public Produto findById(Integer id) {
+
+		try {
+			Produto produto = repository.findById(id).get();
+			return produto;
+
+		} catch (NoSuchElementException e) {
+			throw new ObjectNotFoundException("Produto não encotrado na base de dados.");
+		}
+	}
+
+	public Produto insert(Produto produto) {
+		return repository.save(produto);
+	}
+
+	public Produto update(Produto produto, Integer id) {
+
+		try {
+			Produto obj = repository.getOne(id); // return a reference to the entity
+			obj.setId(produto.getId());
+			obj.setNome(produto.getNome());
+			obj.setDescricao(produto.getDescricao());
+			obj.setPrecoUnitario(produto.getPrecoUnitario());
+			obj.setUnidade(produto.getUnidade());
+			obj.setCategoriaId(produto.getCategoriaId());
+			obj.setFornecedorId(produto.getFornecedorId());
+			obj.setMarcaId(produto.getMarcaId());
+			
+			return repository.save(obj);
+
+		} catch (EntityNotFoundException e) {
+			throw new ObjectNotFoundException("Produto não encontrado na base de dados para atualização.");
+		}
+
+	}
+
+	public void delete(Integer id) {
+		try {
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundException("Produto não encontrado na base de dados para deleção.");
+		}
+	}
+}
