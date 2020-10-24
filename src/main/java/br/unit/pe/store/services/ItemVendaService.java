bce13001d3404be93033c870a0921daa5e74106d
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-10-23 21:43:08 -0300

    ItemVenda entity service layer

---
+++ src/main/java/br/unit/pe/store/services/ItemVendaService.java
@@ -0,0 +1,68 @@
+package br.unit.pe.store.services;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import br.unit.pe.store.domain.ItemVenda;
+import br.unit.pe.store.repositories.ItemVendaRepository;
+import br.unit.pe.store.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ItemVendaService {
+	
+	@Autowired
+	private ItemVendaRepository repository;
+	
+	public List<ItemVenda> findAll(Pageable pageable) {
+		return repository.findAll(pageable).getContent();
+	}
+	
+	public ItemVenda findById(Integer id) {
+
+		try {
+			ItemVenda itemVenda = repository.findById(id).get();
+			return itemVenda;
+
+		} catch (NoSuchElementException e) {
+			throw new ObjectNotFoundException("Item de Venda não encotrado na base de dados.");
+		}
+	}
+
+	public ItemVenda insert(ItemVenda itemVenda) {
+		return repository.save(itemVenda);
+	}
+
+	public ItemVenda update(ItemVenda itemVenda, Integer id) {
+
+		try {
+			ItemVenda obj = repository.getOne(id); // return a reference to the entity
+			obj.setId(itemVenda.getId());
+			obj.setProdutoId(itemVenda.getProdutoId());
+			obj.setVendaId(itemVenda.getVendaId());
+			obj.setQuantidade(itemVenda.getQuantidade());
+			obj.setValorUnitario(itemVenda.getValorUnitario());
+			
+			return repository.save(obj);
+
+		} catch (EntityNotFoundException e) {
+			throw new ObjectNotFoundException("Item de Venda não encontrado na base de dados para atualização.");
+		}
+
+	}
+
+	public void delete(Integer id) {
+		try {
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundException("Item de Venda não encontrado na base de dados para deleção.");
+		}
+	}
+}
