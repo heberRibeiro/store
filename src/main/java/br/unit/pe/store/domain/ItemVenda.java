8c43ebc6d15403018ac782b9e407eb9e755099cf
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2021-01-23 17:21:42 -0300

    Adjust in item venda

--- src/main/java/br/unit/pe/store/domain/ItemVenda.java
+++ src/main/java/br/unit/pe/store/domain/ItemVenda.java
@@ -86,6 +86,14 @@ public class ItemVenda implements Serializable {
 		this.produto = produto;
 	}
 
+	public Venda getVenda() {
+		return venda;
+	}
+
+	public void setVenda(Venda venda) {
+		this.venda = venda;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
